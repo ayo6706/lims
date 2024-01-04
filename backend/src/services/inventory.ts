bfb1e00fd72e3869a4d372f2a6399f9e461b7953
Ayomide Onibokun <ayo6706@gmail.com>
2024-01-04 22:32:11 +0100

    implement optimize stock levels

--- backend/src/services/inventory.ts
+++ backend/src/services/inventory.ts
@@ -1,21 +1,21 @@
 import { InventoryRepository } from "../repository/inventory/inventory.repository";
 import { failedPromise } from "./util";
 
-export default class InventoryService{
-    constructor(private readonly repo: InventoryRepository){
+export default class InventoryService {
+    constructor(private readonly repo: InventoryRepository) {
     }
 
     async checkAndGeneratePurchaseOrders(): Promise<void> {
-        try{
+        try {
             const items = await this.repo.getInventories();
             items.forEach(async (item) => {
-                if (item.currentStock <= item.reorderLevel) {
+                if (item.currentStock! <= item.reorderLevel!) {
                     const expectedDeliveryDate = new Date(
-                        Date.now() + item.leadTimeDays * 24 * 60 * 60 * 1000
-                        );
+                        Date.now() + item.leadTimeDays! * 24 * 60 * 60 * 1000
+                    );
                     const data = {
                         inventoryItemId: item.id!,
-                        quantityOrdered: item.optimalStockLevel - item.currentStock,
+                        quantityOrdered: item.optimalStockLevel! - item.currentStock!,
                         orderDate: new Date(),
                         expectedDeliveryDate,
                         status: 'Ordered',
@@ -23,7 +23,40 @@ export default class InventoryService{
                     await this.repo.createPurchaseOrder(data)
                 }
             });
-        }catch(error: any){
+        } catch (error: any) {
+            return failedPromise(error)
+        }
+    }
+
+    async optimizeStockLevels(): Promise<void> {
+        try {
+
+            const data = await this.repo.getInventories();
+
+            for (let item of data) {
+                // Calculate average sales velocity
+                let totalQuantityChange = 0;
+                item.inventoryHistory!.forEach(history => {
+                    // Assuming a negative change indicates a sale
+                    if (history.quantityChange < 0) totalQuantityChange -= history.quantityChange; // aggregate sold items
+                });
+
+                const salesVelocity = totalQuantityChange / item.inventoryHistory!.length;
+
+                // Adjust reorder level based on the new sales velocity and some buffer 
+                // (e.g., 20% more than average sales)
+                const newReorderLevel = Math.ceil(salesVelocity * (1 + 0.20));
+
+                // Update the item if the calculated reorder level is different
+                if (newReorderLevel !== item.reorderLevel) {
+                    const updateObj = {
+                        id: item.id,
+                        reorderLevel: newReorderLevel
+                    }
+                    await this.repo.updateInventory(updateObj)
+                }
+            }
+        } catch (error: any) {
             return failedPromise(error)
         }
     }
